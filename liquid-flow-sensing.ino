cae5174124715b94b5a5da428727e5d69da0c653
Badar Wildani <wildanie12@gmail.com>
2022-07-31 20:06:01 +0700

    add: wifi connection sample

--- liquid-flow-sensing.ino
+++ liquid-flow-sensing.ino
@@ -1,13 +1,26 @@
+#include <ESP8266WiFi.h>
+const char *ssid = "HUAWEI-AY498A";
+const char *password = "15764233";
 void setup()
 {
-    pinMode(D0, OUTPUT);
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("Connecting to WiFi...");
+        delay(500);
+    }
+
+    Serial.println("Connected WiFi");
+    Serial.print("SSID: ");
+    Serial.print(WiFi.SSID());
+    Serial.printf("IP: ");
+    Serial.print(WiFi.localIP());
 
 }
 
 void loop()
 {
-    digitalWrite(ledPin, HIGH);
-    delay(2000);
-    digitalWrite(ledPin, LOW);
-    delay(2000);
+    client.loop();
 }
