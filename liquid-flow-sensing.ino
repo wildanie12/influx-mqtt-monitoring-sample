0acce449767ba4ed86016607d739f7a9c9c2e94f
Badar Wildani <wildanie12@gmail.com>
2022-07-31 20:06:32 +0700

    feat: mqtt publish implementation

--- liquid-flow-sensing.ino
+++ liquid-flow-sensing.ino
@@ -1,6 +1,15 @@
+#include <PubSubClient.h>
 #include <ESP8266WiFi.h>
+
 const char *ssid = "HUAWEI-AY498A";
 const char *password = "15764233";
+
+const char *mqttHost = "10.0.0.2";
+const int mqttPort = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 void setup()
 {
     Serial.begin(115200);
@@ -18,6 +27,18 @@ void setup()
     Serial.printf("IP: ");
     Serial.print(WiFi.localIP());
 
+    client.setServer("10.0.0.2", 1883);
+    client.connect("ESP8266Client", NULL, NULL);
+    if (!client.connected())
+    {
+        Serial.print("failed with state ");
+        Serial.print(client.state());
+        delay(2000);
+    }
+    Serial.println("mqtt client is connected");
+
+    client.subscribe("test");
+    client.publish("test", "ngok this is from esp");
 }
 
 void loop()
